018d02804b8990887cff802b7056ea9973c100b4
Moha Codes <112027511+Mohamed-code-ship@users.noreply.github.com>
2023-10-22 10:19:00 +0300

    ADD .  chain.py

--- chain.py
+++ chain.py
@@ -1,18 +1,16 @@
 import os
-import sys
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.llms import HuggingFaceHub
 from langflow.chat import LangflowChat
-from langflow.models import HuggingFaceModel
 
+# Custom Langchain tool
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -20,7 +18,6 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
-
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
@@ -32,32 +29,20 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-
 def get_vectorstore(text_chunks):
-    openai_api_key = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-    
-    if not openai_api_key: 
-        raise ValueError("OpenAI API key not found in environment variables.")
-    
+    # openai_api_key = os.getenv("OPENAI_API_KEY")
+    # if not openai_api_key:
+    #     raise ValueError("OpenAI API key not found in environment variables.")
+    # embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+# Monkey-patching the functions
+custom_langchain_tool = locals().copy()
 
-def get_conversation_chain(vectorstore):
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-    chat = LangflowChat(model=llm)
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-
-    conversation_chain = ConversationalRetrievalChain.from_chat(
-        chat=chat,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
-    )
-    return conversation_chain
-
-
+# Main script
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
@@ -67,7 +52,6 @@ def handle_userinput(user_question):
             st.write(f"User: {message.content}")
         else:
             st.write(f"Bot: {message.content}")
-
 
 def main():
     load_dotenv()
@@ -88,11 +72,11 @@ def main():
         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                vectorstore = get_vectorstore(text_chunks)
-                st.session_state.conversation = get_conversation_chain(vectorstore)
-
+                raw_text = custom_langchain_tool['get_pdf_text'](pdf_docs)
+                text_chunks = custom_langchain_tool['get_text_chunks'](raw_text)
+                vectorstore = custom_langchain_tool['get_vectorstore'](text_chunks)
+                # st.session_state.conversation = custom_langchain_tool['get_conversation_chain'](vectorstore)
 
 if __name__ == '__main__':
     main()
+
